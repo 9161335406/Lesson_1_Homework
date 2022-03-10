7df8e98797a2c5f7b90253d3ab8559b0b0acc18f
geekbrains <4091709@mail.ru>
2022-03-10 22:15:01 +0300

    Отлажено, запускается.

--- lessonHomework_3_1/Program.cs
+++ lessonHomework_3_1/Program.cs
@@ -7,16 +7,23 @@ namespace lessonHomework_3_1
         static void Main(string[] args)
         {
             int[,] array = new int[5, 5];
-             for (int i = 0; i < 5; i++) {
-                for (j = 1; j < 5; j++)
+            array[0, 1] = 1;
+            array[1, 2] = 1;
+            array[2, 3] = 1;
+            array[3, 4] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 1; j < 5; j++)
                 {
                     Console.Write(array[i, j] + "  ");
                 }
                 Console.WriteLine();
-            
 
-            
 
+
+
+            }
+            Console.ReadKey();
         }
     }
 
