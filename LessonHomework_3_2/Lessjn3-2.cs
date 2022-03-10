cf33291467d4374987b3604ac4ee262600c84237
geekbrains <4091709@mail.ru>
2022-03-11 00:39:48 +0300

    В процессе...

---
+++ LessonHomework_3_2/Lessjn3-2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonHomework_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] contacts = new string[5, 2]
+           {
+           {"Sem", "phone/email" } , {"Curt" , "phone/email"} , {"Tom", {"phone/email"} ,
+
+           {"Helly", "phone/email" } , {"Yulya", "phone/email"};
+           }
+            Console.
+
+
+
+        }
+    }
+}
