110aba9e3af33b6fd7218fe6b1f6d37c55ed2f34
geekbrains <4091709@mail.ru>
2022-03-12 02:35:14 +0300

    ДЗ № 3-3  выполнено

---
+++ LessonHomework_3_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonHomework_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello world";
+            string rev = "";
+            for (int i = 1; i <= text.Length; i++)
+            {
+                rev += text[text.Length - i]; //Запуск строки в обратном порядке
+            }
+            Console.WriteLine(rev);
+
+            Console.ReadKey();
+        }
+    }
+}
