0fe191375d4c3bbcface95227ae44779d22def88
geekbrains <4091709@mail.ru>
2022-03-12 02:45:02 +0300

    Конечный результат.

--- LessonHomework_3_3/Program.cs
+++ LessonHomework_3_3/Program.cs
@@ -11,6 +11,7 @@ namespace LessonHomework_3_3
         static void Main(string[] args)
         {
             string text = "Hello world";
+            Console.WriteLine(text);
             string rev = "";
             for (int i = 1; i <= text.Length; i++)
             {
