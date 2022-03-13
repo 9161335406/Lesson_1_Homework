d08e50516d74e947d438603bfa140db97d341cde
geekbrains <4091709@mail.ru>
2022-03-13 19:49:43 +0300

    Морской бой

--- LessonHomework_3_4/Program.cs
+++ LessonHomework_3_4/Program.cs
@@ -10,18 +10,53 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            int[,] Pole = new int[11, 11];
-            int rows = Pole.GetUpperBound(0);
-            int columns = Pole.Length / rows;//поле боя
-
-            for (int i = 0; i < rows; i++) ;
+            int[,] Pole = new int[10, 10];
+            int rows = Pole.GetLength(0);
+            int columns = Pole.GetLength(1);
+            //Однопалубные корабли
+            Pole[2, 2] = 1;
+            Pole[8, 2] = 1;
+            Pole[0, 5] = 1;
+            Pole[9, 0] = 1;
+            //Двухпалубные корабли
+            Pole[3, 4] = 1;
+            Pole[3, 2] = 1;
+            Pole[6, 4] = 1;
+            Pole[6, 5] = 1; 
+            Pole[8, 4] = 1; 
+            Pole[8, 5] = 1;
+            //Трёхпалубные корабли
+            Pole[2, 8] = 1;
+            Pole[3, 8] = 1;
+            Pole[4, 8] = 1;
+            Pole[9, 9] = 1;
+            Pole[8, 9] = 1;
+            Pole[7, 9] = 1;
+            //Четырёхпалубный корабль
+            Pole[0, 0] = 1;
+            Pole[1, 0] = 1;
+            Pole[2, 0] = 1;
+            Pole[3, 0] = 1;
+            for (int i = 0; i < columns; i++) 
             {
-                for (int j = 0; j < columns; j++) ;
-                Console.Write($"{Pole[rows, columns]}");
+                for (int j = 0; j < rows; j++)
+                {
+                   
+                    if (Pole[i, j] != 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write(0 + " ");
+                    }
+                    else 
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                       Console.Write("X ");
+                    }
+                   
+                }
+                Console.WriteLine();   
             }
             Console.ReadKey();
-
-
         }
     }
 }
