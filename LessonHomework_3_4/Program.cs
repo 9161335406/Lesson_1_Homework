c0792f72c5bb15e3979a108f9827fe61aa584b99
geekbrains <4091709@mail.ru>
2022-03-13 13:10:00 +0300

    морской бой

---
+++ LessonHomework_3_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Sea_​​battle
+    {
+        static void Main(string[] args)
+        {
+            int[,] Pole = new int[11, 11];
+            int rows = Pole.GetUpperBound(0);
+            int columns = Pole.Length / rows;//поле боя
+
+            for (int i = 0; i < rows; i++) ;
+            {
+                for (int j = 0; j < columns; j++) ;
+                Console.Write($"{Pole[rows, columns]}");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
