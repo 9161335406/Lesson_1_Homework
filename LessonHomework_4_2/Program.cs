1d60116b216130016b0ab44c1f7843b046a65fd8
geekbrains <4091709@mail.ru>
2022-04-02 17:38:53 +0300

    процесс отладки

---
+++ LessonHomework_4_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonHomework_4_2
+{
+    class Program
+    {
+        static int Sum(int[] numbArray)
+        {
+            int result = numbArray.Sum();
+
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите кол-во элементов массива:\t");
+
+            int elementsCount = int.Parse(Console.ReadLine());
+
+            int[] numbArray = new int[elementsCount];
+
+            for (int i = 0; i < numbArray.Length; i++)
+            {
+             Console.Write($"\nВведите элемент массива {i}:\t ");
+
+             numbArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Вывод элементов массива:");
+
+            for (int i = 0; i < numbArray.Length; i++)
+            {
+                               
+                Console.Write(" ");
+
+                Console.Write(numbArray[i]);
+            
+            }
+
+            int c = numbArray.Sum();
+
+            Console.Write(c);
+
+
+                Console.ReadKey();
+        }
+
+    }
+}
---
+++ LessonHomework_4_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LessonHomework_4_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LessonHomework_4_2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("69cd00ef-c79e-4bb4-9e66-6b9d10105641")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
