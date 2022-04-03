cb86c4df078d89d2719b8a7d402c165a4d6c44da
geekbrains <4091709@mail.ru>
2022-04-03 22:51:51 +0300

    Вычисление ряда Фибоначчи с помощью рекурсии

---
+++ LessonHomework_4_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonHomework_4_4
+{
+    class Fibonacci
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число ряда Фибоначчи: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{Fibonaththi(a)}");//Вводим число фибоначчи
+            Console.ReadKey();
+        }
+
+        public static int Fibonaththi(int n)//Метод рекурсии числа фибоначчи
+        {
+            if (n == 0)
+            {
+                return 0;
+            } 
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else { 
+
+            return Fibonaththi(n - 1) + Fibonaththi(n - 2);
+
+            }
+
+        }
+    }
+}
+
+
+
---
+++ LessonHomework_4_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LessonHomework_4_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LessonHomework_4_4")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("af473e90-8905-4a9b-9bf1-287827ba7ab5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
