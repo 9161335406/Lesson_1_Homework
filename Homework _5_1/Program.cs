148b9c1615cd2c16af4b9475c4b0ab3224b28d48
geekbrains <4091709@mail.ru>
2022-04-07 23:55:42 +0300

    Текстовый файл и время

---
+++ Homework _5_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework__5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "file_txt;";
+            string time = DateTime.Now.ToLongTimeString();//Текущее время
+            File.WriteAllText(filename,"str");
+            File.AppendAllText(filename, "time");
+            Console.WriteLine($"{filename}, {time}");
+            Console.ReadKey();
+        } 
+       
+    }
+}
---
+++ LessonHomework_5_1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+public class Files
+{
+    public Files()
+	{
+        string filename = text.text.file
+
+        File.WriteAllText(filename, "str");
+
+        File.AppendAllText(filename, 
+
+
+    }
+}
