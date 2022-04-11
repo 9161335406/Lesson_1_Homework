f4aa19caae64bffd4b1249f527f1a39cf81cfe0c
geekbrains <4091709@mail.ru>
2022-04-11 12:20:27 +0300

    Скомпилировано и прокомментировано. Выполнено.

---
+++ LessonHomework_5_3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonHomework_5_3
+{
+    class Program
+    {
+       public static void Main(string[] args)
+       {//Вводим с косоли произвольные числа от 0 до 255 и записываем в их в файл.
+            Console.Write("Введите кол- во элементов массива :\t");
+            byte elements = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Введите числа от 0 до 255:\t");
+            {
+                byte[] array = new byte[elements];//Объявляем массив.
+
+                for (byte i = 0; i < array.Length; i++)
+                {
+                    array[i] = byte.Parse(Console.ReadLine());//Ввод значений массива.
+                }
+                File.WriteAllBytes("bytes.bin", array);//Записываем массив в файл.
+                _ = File.ReadAllBytes("bytes.bin");
+
+                for (byte i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");//показ полученного массива.
+                }
+
+                    Console.ReadKey();
+            }
+
+       }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
