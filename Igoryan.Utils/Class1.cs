7076df6fb4520f700f0a35f0a1b75b5ca4557f55
geekbrains <4091709@mail.ru>
2022-04-16 02:20:28 +0300

    ДЗ Урока № 8

---
+++ Igoryan.Utils/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igoryan.Utils
+{
+    public static class Utils
+    {
+        public static void PrintInfo(int homeworkNumber, string fio)
+        {
+            Console.WriteLine($"Домашняя работа. Урок {homeworkNumber}");
+            Console.WriteLine($"Студент {fio}");
+            Console.WriteLine("====================================");
+        }
+    }
+}
+
---
+++ LessonHomework_8/Sample01.cs
@@ -0,0 +1,35 @@
+﻿using Igoryan.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonHomework_8
+{
+    internal class Sample01
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = Properties.Settings.Default.ApplicationName;
+
+            Utils.PrintInfo(8, "Разумных Игорь Александрович");
+
+            Console.WriteLine(Properties.Settings.Default.Usersettings1);
+            Console.WriteLine(Properties.Settings.Default.Usersettings2);
+
+            Properties.Settings.Default.Usersettings1 = "Hello student";
+            Properties.Settings.Default.Usersettings2 = 1000;
+            Properties.Settings.Default.Save();
+
+            Console.WriteLine(Properties.Settings.Default.Usersettings1);
+            Console.WriteLine(Properties.Settings.Default.Usersettings2);
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
+
+
+
