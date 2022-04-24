c4f94ca3bfbf1f13c5270308d9026915312b0cb8
geekbrains <4091709@mail.ru>
2022-04-24 21:29:26 +0300

    Доработка  программы.

--- FileMeneger/Program.cs
+++ FileMeneger/Program.cs
@@ -10,6 +10,8 @@ namespace FileMeneger
         const int WINDOW_WIDTH = 120;
         private static string currentDir = Directory.GetCurrentDirectory();
 
+        public static string destinationDir { get; private set; }
+
         static void Main(string[] args)
         {
             Console.BackgroundColor = ConsoleColor.DarkBlue;
@@ -84,188 +86,194 @@ namespace FileMeneger
         static void ParseCommandString(string command)
         {
             string[] commandParams = command.ToLower().Split(' ');
-            
 
-          if (commandParams.Length > 0)
-          {
-            switch (commandParams[0])
+
+            if (commandParams.Length > 0)
             {
-               case "cd":
-               
-               if (commandParams.Length > 1 && Directory.Exists(commandParams[1]))
-               {
-                  currentDir = commandParams[1];
-               }
-
-               break;
-                   
-                case "ls":
-                           
-                if (commandParams.Length > 1 && Directory.Exists(commandParams[1]))
+                string dir = null;
+                switch (commandParams[0])
                 {
-                   if (commandParams.Length > 3 && commandParams[2] == "-p" && int.TryParse(commandParams[3], out int n))
-                   {
-                      DrawTree(new DirectoryInfo(commandParams[1]), n);
-                   }
-                   else
-                   {
-                      DrawTree(new DirectoryInfo(commandParams[1]), 1);
-                   }
-                }
-               break;
-            }
+                    case "cd":
 
-          }
-           UpdateConsole();
+                        if (commandParams.Length > 1 && Directory.Exists(commandParams[1]))
+                        {
+                            currentDir = commandParams[1];
+                        }
+
+                        break;
+
+                    case "ls":
+
+                        if (commandParams.Length > 1 && Directory.Exists(commandParams[1]))
+                        {
+                            if (commandParams.Length > 3 && commandParams[2] == "-p" && int.TryParse(commandParams[3], out int n))
+                            {
+                                DrawTree(new DirectoryInfo(commandParams[1]), n);
+                            }
+                            else
+                            {
+                                DrawTree(new DirectoryInfo(commandParams[1]), 1);
+                            }
+                        }
+                        break;
+
+                    case "copy":
+                        {
+                          CopyDirectory();
+                        }
+
+                        break;
+
+                    case "cp":
+
+                        foreach (FileInfo file in dir.GetFiles())
+                        {
+                            string targetFilePath = Path.Combine(destinationDir, file.Name);
+                            file.CopyTo(targetFilePath);
+                        }
+
+                        break;
+
+                    case "del":
+
+                    DirectoryInfo dirInfo = new DirectoryInfo(dir);
+                        if (dirInfo.Exists)
+                        {
+                          dirInfo.Delete(true);
+                        }
+
+                        break;
+                }
+
+            }
+            UpdateConsole();
         }
-            /// <summary>
-            /// Отрисовать дерево каталогов
-            /// </summary>
-            /// <param name="dir">Директория</param>
-            /// <param name="page">Страница</param>
-            static void DrawTree(DirectoryInfo dir, int page)
+
+        private static void CopyDirectory() => throw new NotImplementedException();
+
+        /// <summary>
+        /// Отрисовать дерево каталогов
+        /// </summary>
+        /// <param name="dir">Директория</param>
+        /// <param name="page">Страница</param>
+        static void DrawTree(DirectoryInfo dir, int page)
+        {
+            StringBuilder tree = new StringBuilder();
+            GetTree(tree, dir, "", true);
+            DrawWindow(0, 0, WINDOW_WIDTH, 18);
+            (int currentLeft, int currentTop) = GetCursorPosition();
+            int pageLines = 16;
+            string[] lines = tree.ToString().Split(new char[] { '\n' });
+            int pageTotal = (lines.Length + pageLines - 1) / pageLines;
+            if (page > pageTotal)
+                page = pageTotal;
+
+
+            for (int i = (page - 1) * pageLines, counter = 0; i < page * pageLines; i++, counter++)
             {
-                StringBuilder tree = new StringBuilder();
-                GetTree(tree, dir, "", true);
-                DrawWindow(0, 0, WINDOW_WIDTH, 18);
-                (int currentLeft, int currentTop) = GetCursorPosition();
-                int pageLines = 16;
-                string[] lines = tree.ToString().Split(new char[] { '\n' });
-                int pageTotal = (lines.Length + pageLines - 1) / pageLines;
-                if (page > pageTotal)
-                    page = pageTotal;
-
-
-                for (int i = (page - 1) * pageLines, counter = 0; i < page * pageLines; i++, counter++)
+                if (lines.Length - 1 > i)
                 {
-                    if (lines.Length - 1 > i)
-                    {
-                        Console.SetCursorPosition(currentLeft + 1, currentTop + 1 + counter);
-                        Console.WriteLine(lines[i]);
-                    }
+                    Console.SetCursorPosition(currentLeft + 1, currentTop + 1 + counter);
+                    Console.WriteLine(lines[i]);
                 }
-
-                //Подвал
-
-                string footer = $"╡ {page} of {pageTotal} ╞";
-
-                Console.SetCursorPosition(WINDOW_WIDTH / 2 - footer.Length / 2, 17);
-
-                Console.WriteLine(footer);
             }
 
-            static void GetTree(StringBuilder tree, DirectoryInfo dir, string indent, bool lastDirectory)
+            //Подвал
 
+            string footer = $"╡ {page} of {pageTotal} ╞";
+
+            Console.SetCursorPosition(WINDOW_WIDTH / 2 - footer.Length / 2, 17);
+
+            Console.WriteLine(footer);
+        }
+
+        static void GetTree(StringBuilder tree, DirectoryInfo dir, string indent, bool lastDirectory)
+        {
+            tree.Append(indent);
+            if (lastDirectory)
             {
-                tree.Append(indent);
-                if (lastDirectory)
+                tree.Append("└─");
+                indent += "  ";
+            }
+            else
+            {
+                tree.Append("├─");
+                indent += "│ ";
+            }
+
+            tree.Append($"{dir.Name}\n"); //<---------------------- ПЕРЕХОД НА СЛЕД СТРОКУ
+
+
+            //TODO: Добавляем отображение файлов
+
+            FileInfo[] subFiles = dir.GetFiles();
+
+            for (int i = 0; i < subFiles.Length; i++)
+            {
+                if (i == subFiles.Length - 1)
                 {
-                    tree.Append("└─");
-                    indent += "  ";
+                    tree.Append($"{indent}└─{subFiles[i].Name}\n");
                 }
                 else
                 {
-                    tree.Append("├─");
-                    indent += "│ ";
+                    tree.Append($"{indent}├─{subFiles[i].Name}\n");
                 }
 
-                tree.Append($"{dir.Name}\n"); //<---------------------- ПЕРЕХОД НА СЛЕД СТРОКУ
-
-
-                //TODO: Добавляем отображение файлов
-
-                FileInfo[] subFiles = dir.GetFiles();
-
-                for (int i = 0; i < subFiles.Length; i++)
-                {
-                    if (i == subFiles.Length - 1)
-                    {
-                        tree.Append($"{indent}└─{subFiles[i].Name}\n");
-                    }
-                    else
-                    {
-                        tree.Append($"{indent}├─{subFiles[i].Name}\n");
-                    }
-
-                }
-                DirectoryInfo[] subDirects = dir.GetDirectories();
-
-                for (int i = 0; i < subDirects.Length; i++)
-
-                    GetTree(tree, subDirects[i], indent, i == subDirects.Length - 1);
             }
+            DirectoryInfo[] subDirects = dir.GetDirectories();
 
+            for (int i = 0; i < subDirects.Length; i++)
 
-            /// <summary> 
+                GetTree(tree, subDirects[i], indent, i == subDirects.Length - 1);
+        }
 
-            /// Отрисовать консоль 
+        private static void DrawConsole(string dir, int x, int y, int width, int height)
+        {
+            DrawWindow(x, y, width, height);
+            Console.SetCursorPosition(x + 1, y + height / 2);
+            Console.Write($"{dir}>");
+        }
 
-            /// </summary> 
+        private static void DrawWindow(int x, int y, int width, int height)
+        {
 
-            /// <param name="dir">Текущая директория</param> 
+            Console.SetCursorPosition(x, y);
 
-            /// <param name="x">Начальная позиция по оси X</param> 
+            //header - шапка
 
-            /// <param name="y">Начальная позиция по оси Y</param> 
+            Console.Write("╔");
 
-            /// <param name="width">Ширина</param> 
+            for (int i = 0; i < width - 2; i++)
 
-            /// <param name="height">Высота</param> 
+                Console.Write("═");
 
-            static void DrawConsole(string dir, int x, int y, int width, int height)
+            Console.Write("╗");
+
+            Console.SetCursorPosition(x, y + 1);
+
+            for (int i = 0; i < height - 2; i++)
             {
-                DrawWindow(x, y, width, height);
-                Console.SetCursorPosition(x + 1, y + height / 2);
-                Console.Write($"{dir}>");
-            }
+                Console.Write("║");
 
-            /// <summary>
-            /// Отрисовать окно
-            /// </summary>
-            /// <param name="x">Начальная позиция по оси Х</param>
-            /// <param name="y">Начальная позиция по оси У</param>
-            /// <param name="width">Ширина</param>
-            /// <param name="height">Высота</param>
-
-            static void DrawWindow(int x, int y, int width, int height)
-            {
-
-                Console.SetCursorPosition(x, y);
-
-                //header - шапка
-
-                Console.Write("╔");
-
-                for (int i = 0; i < width - 2; i++)
-
-                    Console.Write("═");
-
-                Console.Write("╗");
-
-                Console.SetCursorPosition(x, y + 1);
-
-                for (int i = 0; i < height - 2; i++)
+                for (int j = x + 1; j < x + width - 1; j++)
                 {
-                    Console.Write("║");
-
-                    for (int j = x + 1; j < x + width - 1; j++)
-                    {
-                        Console.Write(" ");
-                    }
-                    Console.Write("║");
+                    Console.Write(" ");
                 }
-                //footer - подвал
-
-                Console.Write("╚");
-
-                for (int i = 0; i < width - 2; i++)
-
-                    Console.Write("═");
-
-                Console.Write("╝");
-
-                Console.SetCursorPosition(x, y);
+                Console.Write("║");
             }
-        
+            //footer - подвал
+
+            Console.Write("╚");
+
+            for (int i = 0; i < width - 2; i++)
+
+                Console.Write("═");
+
+            Console.Write("╝");
+
+            Console.SetCursorPosition(x, y);
+
+        }
+
     }
 }
